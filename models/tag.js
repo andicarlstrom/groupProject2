6c7ba29b45805074523173555ce04ba1a6016788
PenajTokyo <penajtokyorm@gmail.com>
2018-11-11 12:10:46 -0700

    My current branch trying to merge

---
+++ models/tag.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes) {
+    var Tag = sequelize.define("Tag", {
+        style: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        color: {
+            type: DataTypes.ENUM("color", "black and white"),
+            allowNull: false
+        },
+        bodypart: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    Tag.associate = function(models) {
+        Tag.belongsTo(models.Picture, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    }
+    return Tag;
+};
+console.log("Tag")
