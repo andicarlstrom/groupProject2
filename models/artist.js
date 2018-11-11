f6a859055f4662b9233c9b2ad99ba7729ae42abd
PenajTokyo <penajtokyorm@gmail.com>
2018-11-11 11:46:35 -0700

    the working database and models

---
+++ models/artist.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+    var Artist = sequelize.define ("Artist", {   
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pricing: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }     
+    });
+
+    Artist.associate = function(models) {
+        Artist.hasOne(models.User);
+
+        Artist.hasMany(models.Picture, {
+            onDelete : "cascade"
+        });
+    };
+    
+    return Artist;
+    }
+   
+console.log("Artist")
