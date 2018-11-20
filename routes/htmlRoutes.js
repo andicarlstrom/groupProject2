3196dd494c29419fa1d67bfe82cfb91bb9c06210
Andrea Carlstrom <andi.carlstrom@gmail.com>
2018-11-20 10:30:31 -0700

    Updated artist profile styling and logic & a plus (+) button logic that only displays on artist profiles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,17 +4,8 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     console.log(req.session);
-    // db.Example.findAll({}).then(function(dbExamples) {
-    //   res.render("index", {
-    //     msg: "Welcome!",
-    //     examples: dbExamples
-    //   });
-    // });
 
-    res.render("index", {
-      msg: "Welcome!",
-      examples: []
-    });
+    res.render("index", {});
   });
 
   // html route that generates the tattoo artists profile
@@ -36,11 +27,20 @@ module.exports = function(app) {
         zip,
         pricing,
         specialization,
+        photos,
         CustomerId,
         Customer
       } = dbObject;
 
-      const { firstName, lastName } = Customer.dataValues;
+      const { firstName, lastName, type } = Customer.dataValues;
+
+      // console.log("customer.datavalues: ", Customer.dataValues);
+      let showAdd;
+      if (type === "artist") {
+        showAdd = true;
+      } else {
+        showAdd = false;
+      }
 
       res.render("artistProfile", {
         location,
@@ -52,46 +52,14 @@ module.exports = function(app) {
         specialization,
         id: CustomerId,
         firstName,
-        lastName
+        lastName,
+        showAdd: showAdd,
+        photos: photos
       });
     });
   });
 
-  //Get artist profile page. Is this necessary based on HTML route for same page above? Will this pull the images?
-  app.get("/api/artist-profile/:id", function(req, res) {
-    db.Artist.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.User]
-    })
-      .then(function(dbArtist) {
-        res.json(dbArtist);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
-
-  //HTML route for templating the image form modal.
-  app.get("/image-form", function(req, res) {
-    res.render("image-form");
-  });
-
-  //Create a new image by artist. Does this make it so that it is associated with correct artist?
-  app.post("/api/image-form/:id", function(req, res) {
-    var newImage = req.body;
-    console.log(res.body);
-    res.json("good job!!"); //This is not real for production
-
-    db.Picture.create(newImage, { include: [db.Tag] })
-      .then(function(dbPicture) {
-        res.json(dbPicture);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
+  // ----- Here is where the images routes will go -----
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
